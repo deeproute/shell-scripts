5cd14b6e15ab36350316e89860ef4e83cb0816c0
deeproute <13484141+deeproute@users.noreply.github.com>
2022-01-31 16:13:20 +0000

    Change URL

--- src/lib/network/network-functions.sh
+++ src/lib/network/network-functions.sh
@@ -5,9 +5,9 @@ setProxy()
     local -r enable_proxy="${1}"; shift
 
     if [[ "${enable_proxy}" ]] ; then
-        export HTTP_PROXY=http://proxylb.internal.epo.org:8080
+        export HTTP_PROXY=http://proxylb.internal.company.org:8080
         export HTTPS_PROXY=$HTTP_PROXY
-        export NO_PROXY=.internal.epo.org,localhost,127.0.0.1
+        export NO_PROXY=.internal.company.org,localhost,127.0.0.1
         export http_proxy=$HTTP_PROXY
         export https_proxy=$HTTP_PROXY
         export no_proxy=$NO_PROXY
